9d470a852a907fef2b9bbf11f0d1883152ff6e34
BassSnake196 <cgerardo@ucol.mx>
2025-09-08 14:49:24 -0600

    prubea

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'todo-app-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
+];
+
+// Instalar y cachear assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Estrategia offline-first: intenta cache, luego red
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
+
+// Limpiar caches viejos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
